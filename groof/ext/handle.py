b24a4f149cc003f98468819e2da26e3ab267709a
levch <levchenko.d.a1998@gmail.com>
2022-08-15 12:21:48 +0300

    .

--- groof/ext/handle.py
+++ groof/ext/handle.py
@@ -1,7 +1,7 @@
-from typing import Callable
+from typing import Callable, Any
 
-Handler = Callable[[], ...]
-Decorator = Callable[[Handler], ...]
+Handler = Callable[[], Any]
+Decorator = Callable[[Handler], Any]
 
 
 def handle(event: Decorator, callback: Handler):
